00eaa5441adbeadff10e21f597a1d0e60a6ec82b
FireTheLost <prajwalmsreddy@gmail.com>
2022-01-17 14:28:39 +0530

    Revert "Change String To StringBuilder"
    This reverts commit 81d7d080044f9b7f051882cc35827fbf85459948.

--- src/com/ioipractice/bronze/solutions/Jan16One.java
+++ src/com/ioipractice/bronze/solutions/Jan16One.java
@@ -33,7 +33,7 @@ public class Jan16One {
         PrintWriter pw = new PrintWriter("src/com/ioipractice/bronze/fout/promote.out");
         pw.println(platinumPromoted);
         pw.println(goldPromoted);
-        pw.print(silverPromoted);
+        pw.println(silverPromoted);
         pw.close();
     }
 }
