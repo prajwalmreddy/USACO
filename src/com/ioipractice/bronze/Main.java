42036bd9cb9a6d3f6fb1e84e3c43dcb044f0d499
FireTheLost <prajwalmsreddy@gmail.com>
2022-01-17 11:07:42 +0530

    Add Solution Feb18One

---
+++ src/com/ioipractice/bronze/Main.java
@@ -0,0 +1,11 @@
+/*
+    Tester Class For The Solutions
+    Current Working Directory: C:\Users\prajw\IdeaProjects\USACO
+ */
+
+package com.ioipractice.bronze;
+
+public class Main {
+    public static void main(String[] args) {
+    }
+}
---
+++ src/com/ioipractice/bronze/solutions/Feb18One.java
@@ -0,0 +1,36 @@
+/*
+    Author: Prajwal Reddy
+    Date: January 17th, 2022
+    Problem: USACO 2018 February Contest, Bronze Problem 1. Teleportation
+ */
+
+package com.ioipractice.bronze.solutions;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Feb18One {
+    public static void main(String[] args) throws IOException {
+        byte[] fileBytes = Files.readAllBytes(Paths.get("src/com/ioipractice/bronze/fin/teleport.in"));
+        String input = new String(fileBytes, Charset.defaultCharset());
+
+        String[] numbers = input.split(" ");
+
+        int a = Integer.parseInt(numbers[0]);
+        int b = Integer.parseInt(numbers[1]);
+
+        int x = Integer.parseInt(numbers[2]);
+        int y = Integer.parseInt(numbers[3]);
+
+        int crowDistance = Math.abs(a - b);
+
+        int distance = Math.abs((Math.min(x, y) - Math.min(a, b))) + Math.abs((Math.max(x, y) - Math.max(a, b)));
+
+        PrintWriter pw = new PrintWriter("src/com/ioipractice/bronze/fout/teleport.out");
+        pw.print(Math.min(crowDistance, distance));
+        pw.close();
+    }
+}
